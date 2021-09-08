541759641110d18c2ae8d72fdd0af55e2c6366b2
Pyunji <vusguswl@gmail.com>
2021-09-08 14:26:46 +0900

    0908 <DI> @Resource

--- src/main/java/com/mycompany/webapp/controller/Ch13Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch13Controller.java
@@ -1,5 +1,7 @@
 package com.mycompany.webapp.controller;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.webapp.service.Ch13Service;
 import com.mycompany.webapp.service.Ch13Service1;
 import com.mycompany.webapp.service.Ch13Service2;
 
@@ -17,6 +20,9 @@ public class Ch13Controller {
 
 	private Ch13Service1 ch13Service1;
 	private Ch13Service2 ch13Service2;
+	
+	@Resource
+	private Ch13Service ch13Service; // 해당 인터페이스를 구현한 객체가 대입된다.
 	
 	public Ch13Controller() {
 		logger.info("실행"); 
@@ -54,6 +60,13 @@ public class Ch13Controller {
 	public String request2() {
 		logger.info("실행");
 		ch13Service2.method1();
+		return "redirect:/ch13/content";
+	}
+	
+	@GetMapping("/request3")
+	public String request3() {
+		logger.info("실행");
+		ch13Service.method2();
 		return "redirect:/ch13/content";
 	}
 }
