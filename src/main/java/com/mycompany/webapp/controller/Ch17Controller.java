dc41efeb0e8bc8bb7586b306432cc94a3d188a70
Pyunji <vusguswl@gmail.com>
2021-09-16 17:33:20 +0900

    [0916] [Security] 로그인이 되면 content로 오게, 로그인이 안되면 다시 loginForm으로 가게 하는 것 까지 배움

---
+++ src/main/java/com/mycompany/webapp/controller/Ch17Controller.java
@@ -0,0 +1,24 @@
+package com.mycompany.webapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ch17")
+public class Ch17Controller {
+	private static final Logger logger = LoggerFactory.getLogger(Ch17Controller.class);
+
+	@RequestMapping("/content")
+	public String content() {
+		logger.info("실행");
+
+		return "ch17/content";
+	}
+	
+	@RequestMapping("/loginForm")
+	public String loginForm() {
+		return "ch17/loginForm";
+	}
+}
